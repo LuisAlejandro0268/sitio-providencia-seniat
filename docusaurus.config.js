db65914324cf48b7dccf1273b83a1c6dee443a8d
Luis Alejandro Gimenez <luisale0456@gmail.com>
2026-02-15 17:44:49 -0400

    Actualizar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,16 +19,15 @@ const config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+// Set the production url of your site here
+url: 'https://luisalejandro0268.github.io', // Tu nombre de usuario
+// Set the /<baseUrl>/ pathname under which your site is served
+// For GitHub pages deployment, it is often '/<projectName>/'
+baseUrl: '/sitio-providencia-seniat/', // EL NOMBRE DE TU REPOSITORIO
+// GitHub pages deployment config.
+// If you aren't using GitHub pages, you don't need these.
+organizationName: 'LuisAlejandro0268', // Tu nombre de usuario
+projectName: 'sitio-providencia-seniat', // EL NOMBRE DE TU REPOSITORIO
 
   onBrokenLinks: 'throw',
 
